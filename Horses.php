74799cb7593023d0b5cf6645298dd2404c04fd7d
MrMillard <lpmillard@gmail.com>
2018-12-10 13:31:21 -0800

    Adding a new front page with header and nav bar

--- Horses.php
+++ Horses.php
@@ -1,16 +1,15 @@
-
 	<?php
 		include_once 'Horses.html';
-		require_once 'login.php';
+		require_once 'horse_db_login.php';
 		$conn = mysqli_connect($host, $user, $pass, $db, $port);
 		if($conn->connect_error) die($conn->connect_error);
 		$option = isset($_POST['breed']) ? $_POST['breed'] : false;
 		if ($option){
 			$breed = $_POST['breed'];
-			$query = "	select image, words from Horses where name = " . $breed . ";"
+			$query = "	select image, words from Breeds where name = '" . $breed . "';";
 			$result = mysqli_query($conn, $query);
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo "<img src=" . $row['image'] . "alt='Horse Image'>";
+				echo "<img src='" . $row[image] ."' alt='Horse Image'>";
 				echo $row['words'];
 			}
 		}
--- horse_db_login.php
+++ horse_db_login.php
@@ -2,6 +2,6 @@
     $host = "127.0.0.1";
     $user = "lmillard";
     $pass = "";
-    $db = "horses";
+    $db = "Horses";
     $port = 3306;
  ?>
